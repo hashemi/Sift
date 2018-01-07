49ae753bec4f3e4dc0493c013b58a7df8c601ab6
Ahmad Alhashemi <hashemi@users.noreply.github.com>
2018-01-07 00:44:44 -0500

    Initial Commit

---
+++ Sift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Sift
+//
+//  Created by Ahmad Alhashemi on 2018-01-07.
+//  Copyright © 2018 Ahmad Alhashemi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
