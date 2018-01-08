8b06de67c4df87cd8fdee1cdcdd89ca543f7e266
Ahmad Alhashemi <hashemi@users.noreply.github.com>
2018-01-08 11:11:19 -0500

    First working expression!

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ A Lisp implementation written in Swift, following Christian Queinnec's book [Lis
 
 ## Journal
 
-* 7 Jan 2017 — Near the end of Chapter 1 of the book. The book describes a meta-circular implementation using Lisp/Scheme as the host language. However, given that this a Swift implementation, a few pieces are missing and will need to be implemented. The code currently compiles. The next step is to write an expression and fill in the blanks necessary for it to be evaluated correctly. At this stage, expressions will be constructed in Swift code as we don't have a Lisp lexer and parser yet.
+* 7 Jan 2018 — Near the end of Chapter 1 of the book. The book describes a meta-circular implementation using Lisp/Scheme as the host language. However, given that this a Swift implementation, a few pieces are missing and will need to be implemented. The code currently compiles. The next step is to write an expression and fill in the blanks necessary for it to be evaluated correctly. At this stage, expressions will be constructed in Swift code as we don't have a Lisp lexer and parser yet.
+
+* 8 Jan 2018 — Evaluated the first expression! Swift Arrays cannot capture the nuances of Lisp lists. I changed the `Value` data structure to align better with Lisp's way of doing things and added a few helper functions to convert to and from Swift Array. This may be worth a revisit in the future with an eye towards performance. Next, I will need to look into the mutability story to correctly implement `set-cdr!`.
--- Sift/main.swift
+++ Sift/main.swift
@@ -10,17 +10,23 @@ struct Wrong: Error {
     let message: String
     let value: Value
 
-    init(_ message: String, _ value: Value = .nil) {
+    init(_ message: String, _ value: Value = .null) {
         self.message = message
         self.value = value
     }
 }
 
-struct Atom: Hashable {
+struct Atom: Hashable, ExpressibleByStringLiteral {
+    typealias StringLiteralType = String
+
     private let name: String
 
     init(_ name: String) {
         self.name = name
+    }
+
+    init(stringLiteral: Atom.StringLiteralType) {
+        self.init(stringLiteral)
     }
 
     var hashValue: Int { return name.hashValue }
@@ -32,14 +38,40 @@ struct Atom: Hashable {
 
 struct Vector { }
 
-struct Function {
-    let variables: List<Atom>
+protocol Function {
+    func apply(_ arguments: Value) throws -> Value
+}
+
+struct Lambda: Function {
+    let variables: Value
     let body: Expr
     let env: Environment
 
-    func apply(_ arguments: [Value]) throws -> Value {
+    func apply(_ arguments: Value) throws -> Value {
         var newEnv = try env.extend(variables, arguments)
         return try body.evaluate(env: &newEnv)
+    }
+}
+
+struct NativeFunction: Function {
+    typealias Body = ([Value]) throws -> (Value)
+    
+    let name: Atom
+    let arity: Int
+    let body: Body
+
+    init(_ name: Atom, _ arity: Int, body: @escaping Body) {
+        self.name = name
+        self.arity = arity
+        self.body = body
+    }
+
+    func apply(_ arguments: Value) throws -> Value {
+        let argumentArray = arguments.asArray
+        guard argumentArray.count == arity else {
+            throw Wrong("Incorrect arity", .atom(name) + arguments)
+        }
+        return try body(argumentArray)
     }
 }
 
@@ -57,21 +89,56 @@ enum Value {
     case char(UnicodeScalar)
     case boolean(Bool)
     case vector(Vector)
-    indirect case list(List<Value>)
+    case null
+    indirect case pair(Value, Value)
     case function(Function)
     case expr(Expr)
-
-    static var `nil`: Value { return .list(.empty) }
 
     var boolValue: Bool {
         switch self {
         case .boolean(let bool):
             return bool
         case .atom, .number, .string,
-             .char, .vector, .list,
+             .char, .vector, .null, .pair,
              .function, .expr:
             return true
         }
+    }
+
+    // cons
+    static func + (lhs: Value, rhs: Value) -> Value {
+        return .pair(lhs, rhs)
+    }
+
+    // Swift Array to LISP list
+    init(_ values: [Value]) {
+        self = values.reversed().reduce(Value.null) { list, value in
+            return value + list
+        }
+    }
+
+    // LISP list to Swift Array
+    var asArray: [Value] {
+        var values: [Value] = []
+        var current = self
+        while true {
+            switch current {
+            case let .pair(head, tail):
+                values.append(head)
+                current = tail
+            case .null:
+                return values
+            default:
+                fatalError("Attempted to convert a non-LISP list into a Swift array")
+            }
+        }
+    }
+
+    func car() throws -> Value {
+        guard case let .pair(car, _) = self else {
+            throw Wrong("car: expected pair")
+        }
+        return car
     }
 }
 
@@ -81,7 +148,7 @@ indirect enum Expr {
     case `if`(Expr, Expr, Expr)
     case begin([Expr])
     case set(Atom, Expr)
-    case lambda(List<Atom>, Expr)
+    case lambda(Value, Expr)
     case apply(Expr, [Expr])
 }
 
@@ -101,6 +168,14 @@ struct Environment {
         return env
     }()
 
+    static func defInitial(_ name: Atom, _ value: Value = .atom("void")) {
+        Environment.global.dict[name] = value
+    }
+
+    static func defPrimitive(_ name: Atom, _ arity: Int, body: @escaping NativeFunction.Body) {
+        Environment.global.dict[name] = .function(NativeFunction(name, arity, body: body))
+    }
+
     func lookup(_ atom: Atom) throws -> Value {
         guard let value = dict[atom] else {
             throw Wrong("No such binding", .atom(atom))
@@ -115,30 +190,25 @@ struct Environment {
         dict[atom] = value
     }
 
-    func extend(_ variables: List<Atom>, _ values: [Value]) throws -> Environment {
-        switch variables {
-        case let .plain(list):
-            if list.count < values.count {
-                throw Wrong("Too many values")
-            }
+    func extend(_ variables: Value, _ values: Value) throws -> Environment {
+        switch (variables, values) {
+        case let (.pair(.atom(name), varTail), .pair(value, valTail)):
+            var newDict = dict
+            newDict[name] = value
+            return try Environment(dict: newDict).extend(varTail, valTail)
 
-            if list.count > values.count {
-                throw Wrong("Too less values")
-            }
+        case (.pair, _):
+            throw Wrong("Too less values")
 
-            let newDict = dict.merging(zip(list, values)) { (_, new) in new }
-            return Environment(dict: newDict)
+        case (.null, .null):
+            return self
 
-        case let .dotted(list, dot):
-            if list.count > values.count {
-                throw Wrong("Too less values")
-            }
+        case (.null, _):
+            throw Wrong("Too much values")
 
-            let mapped = zip(list, values[..<list.count])
-            var newDict = dict.merging(mapped) { (_, new) in new }
-
-            let remainder = Array(values[list.count...])
-            newDict[dot] = .list(.plain(remainder))
+        case let (.atom(name), _):
+            var newDict = dict
+            newDict[name] = values
             return Environment(dict: newDict)
         }
     }
@@ -151,7 +221,7 @@ extension Expr {
             switch value {
             case let .atom(atom):
                 return try env.lookup(atom)
-            case .boolean, .number, .string, .char, .vector, .list, .function, .expr:
+            case .boolean, .number, .string, .char, .vector, .null, .pair, .function, .expr:
                 return value
             }
 
@@ -167,14 +237,14 @@ extension Expr {
             for expr in exprs {
                 _ = try expr.evaluate(env: &env)
             }
-            return .nil
+            return .null
 
         case let .set(symbol, value):
             try env.update(symbol, try value.evaluate(env: &env))
-            return .nil
+            return .null
 
         case let .lambda(variables, body):
-            return .function(Function(variables: variables, body: body, env: env))
+            return .function(Lambda(variables: variables, body: body, env: env))
 
         case let .apply(funcExpr, argExprs):
             let evaluatedFuncExpr = try funcExpr.evaluate(env: &env)
@@ -182,9 +252,38 @@ extension Expr {
             guard case let .function(function) = evaluatedFuncExpr
                 else { throw Wrong("Not a function", evaluatedFuncExpr) }
 
-            let arguments = try argExprs.map { try $0.evaluate(env: &env) }
+            let arguments = Value(try argExprs.map { try $0.evaluate(env: &env) })
 
             return try function.apply(arguments)
         }
     }
 }
+
+Environment.defInitial("foo")
+Environment.defInitial("bar")
+Environment.defInitial("fib")
+Environment.defInitial("fact")
+
+Environment.defPrimitive("cons", 2) { args in
+    return args[0] + args[1]
+}
+
+Environment.defPrimitive("car", 1) { args in
+    return try args[0].car()
+}
+
+let test: Expr =
+    .apply(
+        .value(.atom("car")),
+        [
+            .apply(
+                .value(.atom("cons")),
+                [
+                    .value(.number(1)),
+                    .value(.number(2))
+                ]
+            )
+        ]
+    )
+
+print(try test.evaluate(env: &.global))
